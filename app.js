953bacf3ac4c19bf483bc4b865118841ebd5c39c
John Paguirigan <johnjaroldxvi@gmail.com>
2020-01-23 12:55:55 -0700

    portfolio landing page

---
+++ app.js
@@ -0,0 +1,22 @@
+const words = ['Front End Developer ', "UX & UI Designer", "Coding & Coffee"]
+let count = 0
+let index = 0
+let currentText = ''
+let letter = ''
+
+const type = () => {
+    if (count === words.length) {
+        count = 0;
+    }
+    currentText = words[count]
+    letter = currentText.slice(0, ++index)
+    
+    document.querySelector('.type-writer').textContent = letter;
+    if(letter.length === currentText.length){
+        count++
+        index = 0;
+    }
+    setTimeout(type, 150)
+}
+
+type();
